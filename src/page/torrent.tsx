60646daa8c2b77586beede7978929a363aca77ce
Kieran <kieran@harkin.me>
2023-11-27 20:03:47 +0000

    chore: formatting

--- src/page/torrent.tsx
+++ src/page/torrent.tsx
@@ -29,7 +29,10 @@ export function TorrentDetail({ item }: { item: TaggedNostrEvent }) {
   const login = useLogin();
   const navigate = useNavigate();
   const name = item.tags.find((a) => a[0] === "title")?.at(1);
-  const size = item.tags.filter(a => a[0] === "file").map(a => Number(a[2])).reduce((acc, v) => acc += v, 0);
+  const size = item.tags
+    .filter((a) => a[0] === "file")
+    .map((a) => Number(a[2]))
+    .reduce((acc, v) => (acc += v), 0);
   const files = item.tags.filter((a) => a[0] === "file");
   const tags = item.tags.filter((a) => a[0] === "t").map((a) => a[1]);
 
